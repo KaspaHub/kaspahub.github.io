5457eece021f051aa2ff908fa3ce76e465c6f682
Kaspa Hub <ivoclassic@pm.me>
2025-08-01 19:37:22 +0300

    Updates

--- tools/html-text-editor/assets/summernote-lite.js
+++ tools/html-text-editor/assets/summernote-lite.js
@@ -9436,7 +9436,7 @@ var editor = renderer.create('<div class="note-editor note-frame"></div>');
 var toolbar = renderer.create('<div class="note-toolbar" role="toolbar"></div>');
 var editingArea = renderer.create('<div class="note-editing-area"></div>');
 var codable = renderer.create('<textarea class="note-codable" aria-multiline="true"></textarea>');
-var editable = renderer.create('<div class="note-editable" contentEditable="true" role="textbox" aria-multiline="true"></div>');
+var editable = renderer.create('<div id="text-editor" class="note-editable" contentEditable="true" role="textbox" aria-multiline="true"></div>');
 var statusbar = renderer.create(['<output class="note-status-output" role="status" aria-live="polite"></output>', '<div class="note-statusbar" role="status">', '<div class="note-resizebar" aria-label="resize">', '<div class="note-icon-bar"></div>', '<div class="note-icon-bar"></div>', '<div class="note-icon-bar"></div>', '</div>', '</div>'].join(''));
 var airEditor = renderer.create('<div class="note-editor note-airframe"></div>');
 var airEditable = renderer.create(['<div class="note-editable" contentEditable="true" role="textbox" aria-multiline="true"></div>', '<output class="note-status-output" role="status" aria-live="polite"></output>'].join(''));
