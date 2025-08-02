9860b95d8158aedd130c886c2e0558821fdec747
Kaspa Hub <ivoclassic@pm.me>
2025-08-02 19:09:59 +0300

    Apps

---
+++ app/calculator/assets/service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'v1';
+
+const urlsToCache = [
+  '/tools/calculator/',
+  '/tools/calculator/assets/img/logo-192.png',
+  '/tools/calculator/assets/img/logo-96.png',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(networkResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
