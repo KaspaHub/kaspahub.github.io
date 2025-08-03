9bb9aaebba6a892dd6256eef11cf38314351c908
Kaspa Hub <ivoclassic@pm.me>
2025-08-03 23:00:24 +0300

    Test

--- app/test/scripts/main.js
+++ app/test/scripts/main.js
@@ -1,14 +1,22 @@
+'use strict';
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('sw.js')
-            .then(function (register) {
-                console.log('PWA service worker ready');
-                register.update();
-            })
-            .catch(function (error) {
-                console.log('Register failed! Error:' + error);
-            });
+        navigator.serviceWorker.register('scripts/sw.js').then(function (register) {
+            register.update();
+        });
     });
 }
 
 
+
+
+
+// // Check user internet status (online/offline)
+// function updateOnlineStatus(event) {
+//    if (!navigator.onLine) {
+//        alert('Internet access is not possible!')
+//    }
+// }
+
+// window.addEventListener('online', updateOnlineStatus);
+// window.addEventListener('offline', updateOnlineStatus);
