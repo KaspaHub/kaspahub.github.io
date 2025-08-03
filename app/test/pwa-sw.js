9e6d1be21ee9cf240f084ce547d6a356402b2ff1
Kaspa Hub <ivoclassic@pm.me>
2025-08-04 00:43:57 +0300

    Test

--- app/test/pwa-sw.js
+++ app/test/pwa-sw.js
@@ -1,19 +1,18 @@
 'use strict';
 
-var cache_storage_name = 'pwa-0.1';
+var cache_storage_name = 'pwa-0.2';
 var start_page = '/app/test/';
 var offline_page = '/app/test/offline/';
 var first_cache_urls = [start_page, offline_page];
-var never_cache_urls = [/\/private.html/, /\/panel/, /\/custom-url/];
 
-// Install 
+// Install
 self.addEventListener('install', function (e) {
 	console.log('PWA sw installation');
 	e.waitUntil(caches.open(cache_storage_name).then(function (cache) {
 		console.log('PWA sw caching first urls');
 		first_cache_urls.map(function (url) {
 			return cache.add(url).catch(function (res) {
-				return console.log('PWA: ' + String(res) + ' ' + url);
+				console.log('PWA: ' + String(res) + ' ' + url);
 			});
 		});
 	}));
@@ -30,34 +29,27 @@ self.addEventListener('activate', function (e) {
 			}
 		}));
 	}));
-	return self.clients.claim();
+	self.clients.claim();
 });
 
 // Fetch
 self.addEventListener('fetch', function (e) {
-
-	if (!checkFetchRules(e)) return;
-
-	// Strategy for online user
+	// Online
 	if (e.request.mode === 'navigate' && navigator.onLine) {
 		e.respondWith(fetch(e.request).then(function (response) {
 			return caches.open(cache_storage_name).then(function (cache) {
-				if (never_cache_urls.every(check_never_cache_urls, e.request.url)) {
-					cache.put(e.request, response.clone());
-				}
+				cache.put(e.request, response.clone());
 				return response;
 			});
 		}));
 		return;
 	}
 
-	// Strategy for offline user
+	// Offline
 	e.respondWith(caches.match(e.request).then(function (response) {
 		return response || fetch(e.request).then(function (response) {
 			return caches.open(cache_storage_name).then(function (cache) {
-				if (never_cache_urls.every(check_never_cache_urls, e.request.url)) {
-					cache.put(e.request, response.clone());
-				}
+				cache.put(e.request, response.clone());
 				return response;
 			});
 		});
@@ -65,28 +57,3 @@ self.addEventListener('fetch', function (e) {
 		return caches.match(offline_page);
 	}));
 });
-
-// Check never cache urls 
-function check_never_cache_urls(url) {
-	if (this.match(url)) {
-		return false;
-	}
-	return true;
-}
-
-// Fetch Rules
-function checkFetchRules(e) {
-
-	// Check request url from inside domain.
-	if (new URL(e.request.url).origin !== location.origin) return;
-
-	// Check request url http or https
-	if (!e.request.url.match(/^(http|https):\/\//i)) return;
-
-	// Show offline page for POST requests
-	if (e.request.method !== 'GET') {
-		return caches.match(offline_page);
-	}
-
-	return true;
-}
