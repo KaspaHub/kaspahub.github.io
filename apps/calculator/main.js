9d4d84bce16a6d8509b0d5a7f8a527fc48708818
Kaspa Hub <ivoclassic@pm.me>
2025-08-04 20:53:38 +0300

    Update main.js

--- apps/calculator/main.js
+++ apps/calculator/main.js
@@ -1,21 +1,25 @@
-if (!('serviceWorker' in navigator)) {
-    console.log('[Main] Service workers are not supported in this browser.');
-    return;
+'use strict';
+
+function initServiceWorker() {
+    if (!('serviceWorker' in navigator)) {
+        console.log('[Main] Service workers are not supported in this browser.');
+        return;
+    }
+
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('sw.js')
+            .then(function (register) {
+                register.update();
+                console.log('[Main] Service worker registered.');
+            })
+            .catch(function (error) {
+                console.log('[Main] Service worker registration failed. Error: ' + error);
+            });
+
+        window.addEventListener('online', handleNetworkChange);
+        window.addEventListener('offline', handleNetworkChange);
+    });
 }
-
-window.addEventListener('load', function () {
-    navigator.serviceWorker.register('sw.js')
-        .then(function (register) {
-            register.update();
-            console.log('[Main] Service worker registered.');
-        })
-        .catch(function (error) {
-            console.log('[Main] Service worker registration failed. Error: ' + error);
-        });
-
-    window.addEventListener('online', handleNetworkChange);
-    window.addEventListener('offline', handleNetworkChange);
-});
 
 function handleNetworkChange() {
     if (navigator.onLine) {
@@ -24,3 +28,5 @@ function handleNetworkChange() {
         console.log('[Main] No internet connection.');
     }
 }
+
+initServiceWorker();
