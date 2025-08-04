a6755864e7dbcc22b4783c2c4959a1e654f78e62
Kaspa Hub <ivoclassic@pm.me>
2025-08-04 23:44:28 +0300

    Update main.js

--- apps/calculator/main.js
+++ apps/calculator/main.js
@@ -6,23 +6,20 @@ function initServiceWorker() {
         return;
     }
 
-    window.addEventListener('load', function () {
-        if (navigator.serviceWorker.controller) {
-            console.log('[Main] A service worker is already controlling this page.');
-        }
+    if (navigator.serviceWorker.controller) {
+        console.log('[Main] A service worker is already controlling this page.');
+    }
 
-        navigator.serviceWorker.register('sw.js')
-            .then(function (registration) {
-                console.log('[Main] Service worker registered successfully.');
-            })
-            .catch(function (error) {
-                console.log('[Main] Service worker registration failed:', error);
-            });
+    navigator.serviceWorker.register('sw.js')
+        .then(function (registration) {
+            console.log('[Main] Service worker registered successfully.');
+        })
+        .catch(function (error) {
+            console.log('[Main] Service worker registration failed:', error);
+        });
 
-        window.addEventListener('online', networkChange);
-        window.addEventListener('offline', networkChange);
-    });
-
+    window.addEventListener('online', networkChange);
+    window.addEventListener('offline', networkChange);
 }
 
 function networkChange(event) {
