b9d0642097ff9400de0a9688e65abd3751922683
Kaspa Hub <211249664+Classicgyt@users.noreply.github.com>
2025-08-18 22:40:44 +0300

    Bookmarks

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -26,3 +26,34 @@ function share() {
     alert("Sharing is not supported in this browser.");
   }
 }
+
+
+function toggleBookmark(button) {
+  const name = button.dataset.name;
+  const link = button.dataset.link;
+
+  const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+  const index = bookmarks.findIndex(b => b.link === link);
+  let actionText = '';
+
+  if (index === -1) {
+    bookmarks.unshift({ name, link });
+    if (bookmarks.length > 10) bookmarks.splice(10);
+    actionText = 'Bookmarked!';
+    // console.log(`Bookmarked: "${name}" -> ${link}`);
+  } else {
+    bookmarks.splice(index, 1);
+    actionText = 'Removed!';
+    // console.log(`Removed bookmark: "${name}"`);
+  }
+
+  localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+  const originalText = button.textContent;
+  button.textContent = actionText;
+  button.disabled = true;
+  setTimeout(() => {
+    button.textContent = originalText;
+    button.disabled = false;
+  }, 2000);
+}
