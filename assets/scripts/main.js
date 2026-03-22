4c3ebbc8f7a6645e2a4c94de0ab3f32952791cd8
Kaspa Hub <211249664+Classicxbt@users.noreply.github.com>
2026-03-22 12:46:58 +0200

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -545,7 +545,7 @@ function populateMenu() {
     { name: "Explorer", icon: "🔍", href: "/explorer/" },
     { name: "Overview", icon: "📊", href: "/overview/" },
     { name: "Projects", icon: "💼", href: "/projects/" },
-    { name: "Comparison", icon: "⚡", href: "/speed/" },
+    { name: "Comparison", icon: "⚡", href: "/comparison/" },
     { name: "Donations", icon: "💸", href: "https://tiptr.ee/KaspaHub" }
   ];
 
